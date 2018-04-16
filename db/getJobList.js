f752c3ee6f8cfa0cd9c12298f233e4fec05f7445
lin.b.yang <lin.b.yang@accenture.com>
2018-04-16 16:37:12 +0800

    latest version

--- db/getJobList.js
+++ db/getJobList.js
@@ -2,8 +2,23 @@ var sqlite3 = require('sqlite3').verbose();
 
 exports.getJobData = function (callback) {
     var db = new sqlite3.Database('DBFile/JobHunting.db');
-    const uuidv1 = require('uuid/v1');
     db.all('select * from T_Job', function (err, rows) {
+        callback(rows)
+    });
+    db.close();
+}
+
+exports.getJobDataByID = function (callback, ID) {
+    var db = new sqlite3.Database('DBFile/JobHunting.db');
+    db.all("select * from T_Job where ID = ?", [ID], function (err, rows) {
+        callback(rows)
+    });
+    db.close();
+}
+
+exports.getJobDataByName = function (callback, Name) {
+    var db = new sqlite3.Database('DBFile/JobHunting.db');
+    db.all("select * from T_Job where Name like ?", ['%' + Name + '%'], function (err, rows) {
         callback(rows)
     });
     db.close();
--- routes/joblist.js
+++ routes/joblist.js
@@ -1,12 +1,26 @@
 var express = require('express');
 var router = express.Router();
 var getData = require('../db/getJobList');
+var url = require('url');
+var util = require('util');
 var getRes;
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
     
-    var jobList = getData.getJobData(ResponseBody);
+    getData.getJobData(ResponseBody);
+    getRes = res;
+});
+
+router.get('/ID', function (req, res, next) {
+    var params = url.parse(req.url, true).query;
+    getData.getJobDataByID(ResponseBody, params.ID);
+    getRes = res;
+});
+
+router.get('/Name', function (req, res, next) {
+    var params = url.parse(req.url, true).query;
+    getData.getJobDataByName(ResponseBody, params.Name);
     getRes = res;
 });
 
